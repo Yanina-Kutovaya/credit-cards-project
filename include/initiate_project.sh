73001d0bb886abf732711261da7c9a25bd2280f7
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-07-05 03:42:19 +0300

    Adjust data folder due to DVC tracking

--- include/initiate_project.sh
+++ include/initiate_project.sh
@@ -7,3 +7,25 @@ git push -uf origin main
 kedro new
 cp -r credit-cards-project/* .
 rm -r credit-cards-project
+
+pip install dvc[all]
+dvc init
+git commit -m "Initialize DVC"
+dvc remote add -d s3store s3://credit-cards-data/01_raw
+dvc remote modify s3store endpointurl https://storage.yandexcloud.net
+
+git rm -r --cached 'data'
+git commit -m "stop tracking data"
+git push
+
+dvc add data 
+git add data.dvc
+dvc config core.autostage true
+dvc install
+git push
+
+dvc add data        # after any chages in 'data' folder before commit
+dvc pull            # after git pull to get changes from remote
+
+dvc remove data.dvc # to stop data tracking
+
